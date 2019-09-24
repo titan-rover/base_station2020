9e477a0f1fd471ae43a936ba2a6d7a79e1c72d8e
Gabriel Magallanes <31296957+GabeMags@users.noreply.github.com>
2019-09-23 20:58:18 -0700

    Delete CTestTestfile.cmake
    Cleanup

--- ros9-15/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/Documents/ros9-15/src
-# Build directory: /home/student/Documents/ros9-15/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("simple_test")
