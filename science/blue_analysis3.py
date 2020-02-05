b19132d7bfb7c846256afeddf9b704ab7474aab0
Ryan Luong <ryanluong83@gmail.com>
2020-02-04 22:51:54 -0800

    Updated variable names

--- science/blue_analysis3.py
+++ science/blue_analysis3.py
@@ -10,9 +10,9 @@ from PIL import Image
 
 img_file = Image.open("D3.jpg")
 
-color_region = img_file.crop((1350, 300, 550+1200, 400+300))
-color_region.save("color_region3.jpg")
-image = cv2.imread("color_region3.jpg")
+spec_sample = img_file.crop((1350, 300, 550+1200, 400+300))
+spec_sample.save("spec_sample_3.png")
+image = cv2.imread("spec_sample_3.png")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]
 
@@ -28,10 +28,10 @@ for c in cnts:
     break
 
 cv2.imshow('Cropped #3',ROI)
-cv2.imwrite('cropped3.jpg',ROI)
+cv2.imwrite('crop_spec_sample_3.jpg',ROI)
 cv2.waitKey()
-img_for_graph = Image.open("cropped3.jpg")
-img = img_for_graph.load()
+crop_spec_sample = Image.open("crop_spec_sample_3.png")
+img = crop_spec_sample.load()
 
 [xs, ys] = img_for_graph.size
 max_intensity = 100
