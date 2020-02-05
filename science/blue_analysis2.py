f954d0df9a0f4d0c12b5c9cb10cb5f184276577b
Ryan Luong <ryanluong83@gmail.com>
2020-02-04 22:52:44 -0800

    Updated variable names

--- science/blue_analysis2.py
+++ science/blue_analysis2.py
@@ -11,8 +11,8 @@ from PIL import Image
 img_file = Image.open("D2.jpg")
 
 spec_sample = img_file.crop((1350, 300, 550+1200, 400+300))
-spec_sample.save("spec_sample_2.jpg")
-image = cv2.imread("spec_sample_2.jpg")
+spec_sample.save("spec_sample_2.png")
+image = cv2.imread("spec_sample_2.png")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]
 
@@ -28,9 +28,9 @@ for c in cnts:
     break
 
 cv2.imshow('Cropped #2',ROI)
-cv2.imwrite('crop_spec_sample_2.jpg',ROI)
+cv2.imwrite('crop_spec_sample_2.png',ROI)
 cv2.waitKey()
-crop_spec_sample = Image.open("crop_spec_sample_2.jpg")
+crop_spec_sample = Image.open("crop_spec_sample_2.png")
 img = crop_spec_sample.load()
 
 [xs, ys] = img_for_graph.size
