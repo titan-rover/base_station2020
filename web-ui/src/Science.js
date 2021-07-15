5e27592a7ad87ff0122ef0155a86a1b27c7615f6
GabeMags <magallanes.gabe@gmail.com>
2021-07-14 18:57:05 -0700

    added navbar to science + ryans code

--- web-ui/src/Science.js
+++ web-ui/src/Science.js
@@ -1,32 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { ButtonToolbar } from 'react-bootstrap';
-import {Button} from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+import MyNavbar from './components/Navbar';
 
 // emptyContents() {
-    
+
 // }
 
 class Science extends Component {
     render() {
-        return(
+        return (
             <Container>
-                <Row>
-                    <Col>
-                    <div style={{border: "1px solid green", height: "720px", width: "1280px"}}> Stream
-                        </div>
+                <MyNavbar />
+                <Container>
+                    <Row>
+                        <Col>
+                            <div style={{ border: "1px solid green", height: "720px", width: "1280px" }}> Stream
+                            </div>
                         </Col>
-                    <Col></Col>
-                </Row>
-                <Row>
-                    <Col><div>
-                        <Button variant="danger">Stop Stream</Button>{' '}
-                        <Button variant="primary">Protein Extraction</Button>{' '}
-                        <Button variant="secondary">Fluorescence Microscopy</Button>{' '}</div></Col>
-                </Row>
+                        <Col></Col>
+                    </Row>
+                    <Row>
+                        <Col><div>
+                            <Button variant="danger">Stop Stream</Button>{' '}
+                            <Button variant="primary">Protein Extraction</Button>{' '}
+                            <Button variant="secondary">Fluorescence Microscopy</Button>{' '}</div></Col>
+                    </Row>
+                </Container>
             </Container>
         );
     }
-  }
+}
 
 export default Science;
