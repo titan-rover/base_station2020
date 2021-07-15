dfee761f7261efcfbeae6bcea3ea6bb32eaa0b51
Aucien <blu3sag3@gmail.com>
2021-07-14 17:13:22 -0700

    Updated navbar

--- web-ui/src/components/Navbar.js
+++ web-ui/src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
+import { NavLink} from "react-router-dom";
+import '../css/NavBar.css'
 
 class MyNavbar extends Component {
     render() {
@@ -13,22 +15,26 @@ class MyNavbar extends Component {
 
                     <div class="collapse navbar-collapse" id="navbarColor01">
                         <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="https://controls.titanrover.com">2D Mode
+                            <li class="nav-item">
+                                {/* <a class="nav-link" href="https://controls.titanrover.com">2D Mode
                                 <span class="sr-only">(current)</span>
-                                </a>
+                                </a> */}
+                                <NavLink exact to ="/"className="nav-links"> 2D Mode </NavLink>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="https://controls.titanrover.com/XR">XR Mode</a>
+                                {/* <a class="nav-link" href="https://controls.titanrover.com/XR">XR Mode</a> */}
                                 {/* <a class="nav-link" href="http://localhost:3000/XR">XR Mode</a> */}
+                                <NavLink exact to ="/XR" className="nav-links"> XR </NavLink>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="https://controls.titanrover.com">Science Mode</a>
+                                {/* <a class="nav-link" href="https://controls.titanrover.com">Science Mode</a> */}
                                 {/* <a class="nav-link" href="http://localhost:3000/science">Science</a> */}
+                                <NavLink exact to ="/science" className="nav-links"> Science </NavLink>
                             </li>
                             <li class="nav-item">
                                 {/* <a class="nav-link" href="https://controls.titanrover.com">Electricals Mode</a> */}
-                                <a class="nav-link" href="http://localhost:3000/electricals">Electricals Mode</a>
+                                {/* <a class="nav-link" href="http://localhost:3000/electricals">Electricals Mode</a> */}
+                                <NavLink exact to ="/electricals" className="nav-links"> Electricals </NavLink>
                             </li>
                         </ul>
                     </div>
